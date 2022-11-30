9d2ffca6024be6828d7d3eafc0c39244a87b081e
Ezequiel Muñoz <111892602+Svarium@users.noreply.github.com>
2022-11-30 14:24:42 -0300

    Inicializo proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+/* Servidor */
+
+app.listen(port, () => 'puerto levantado exitosamente')
+
+/* recursos estaticos */
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+/* rutas */
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
